774957914d988295a8596edcc727df9f04156f17
aleksandaralek <aleksandardeveoper@gmail.com>
2023-08-23 21:17:13 +0200

    Updates

---
+++ static/1692818200/movies/565770/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fmovie-app\u002Fstatic\u002F1692818200",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmovies\u002F565770",config:{_app:{basePath:b,assetsPath:b,cdnURL:a}}}}(null,"\u002Fmovie-app\u002F"));
